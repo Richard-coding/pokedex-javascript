71e3de40ea57919d8c07d3be914014c7a8c14468
Richard <richardcoding@outlook.com>
2026-02-25 12:37:35 -0300

    chore: reorganize project structure

---
+++ js/api.js
@@ -0,0 +1,6 @@
+async function getPokemin(pokemon) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  if (!response.ok) throw new Error("Pokemon not found");
+  return response.json();
+}
+
